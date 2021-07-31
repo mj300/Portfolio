15883cb3d6ec4f1375dd34030ecacbd733b62c6f
mj300 <mahdi.joveyni@gmail.com>
2021-07-31 23:45:06 +0100

    fixed

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get_browser('/',function (){
+Route::get('/',function (){
     return 'Ok';
 });
